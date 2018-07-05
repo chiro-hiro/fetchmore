2b428a5d5442a8c9a770a6303217283e562c4b74
Chiro Hiro <chiro@fkguru.com>
2018-07-05 22:54:04 +0700

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # react-ajax
 React Ajax
+
+## Installation
+```
+$ npm i react-ajax
+```
+
+## GET method
+```javascript
+import ajax from 'react-ajax';
+
+ajax({
+    url: 'http://localhost/?'
+}, {
+    username: 'chiro',
+    email: 'chiro@fkguru.com',
+}, console.log, console.warn);
+```
+Result:
+```
+http://localhost/?username=chiro&email=chiro%40fkguru.com
+```
+
+## POST method
+```javascript
+import ajax from 'react-ajax';
+
+ajax({
+    url: 'http://localhost/',
+    method: 'POST'
+}, {
+    username: 'chiro',
+    email: 'chiro@fkguru.com',
+    products: [
+        {id: 3, name: 'cake', price: 2000},
+        {id: 4, name: 'coffe', price: 6000}
+    ]
+}, console.log, console.warn);
+```
+Parsed data:
+```
+username: chiro
+email: chiro@fkguru.com
+products[id][0]: 3
+products[name][0]: cake
+products[price][0]: 2000
+products[id][1]: 4
+products[name][1]: coffe
+products[price][1]: 6000
+```
+Auto encoded data:
+```
+username=chiro&email=chiro%40fkguru.com&products[id][0]=3&products[name][0]=cake&products[price][0]=2000&products[id][1]=4&products[name][1]=coffe&products[price][1]=6000
+```
+
+# License
+This package distributed under [MIT License](https://github.com/chiro-hiro/react-ajax/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,60 @@
+function isEmty(obj) {
+    return (Array.isArray(obj) && obj.length === 0) || (typeof (obj) === 'object' && Object.keys(obj).length === 0);
+}
+
+function buildData(object, data, parentName) {
+    data = data || [];
+    parentName = parentName || '';
+    for (var index in object) {
+        if (Array.isArray(object[index])) {
+            var tmpArray = object[index];
+            for (var i = 0; i < tmpArray.length; i++) {
+                for (var field in tmpArray[i]) {
+                    data.push(encodeURIComponent(index) + '[' + encodeURIComponent(field) + '][' + i.toString() + ']' + '=' + encodeURIComponent(tmpArray[i][field]));
+                }
+            }
+        }
+        else if (typeof (object[index]) === 'object') {
+            buildData(object[index], data, index);
+        }
+        else {
+            if (parentName === '') {
+                data.push(encodeURIComponent(index) + '=' + encodeURIComponent(object[index]));
+            }
+            else {
+                data.push(encodeURIComponent(parentName) + '[' + encodeURI(index) + ']' + '=' + encodeURIComponent(object[index]));
+            }
+
+        }
+    }
+    return data.join('&');
+}
+
+function ajax(request, data, callback, errorCallback) {
+    request = request || {};
+    request.url = request.url || '/';
+    request.method = request.method || 'GET';
+    request.headers = request.headers || {};
+    callback = callback || function () { };
+    errorCallback = errorCallback || function () { };
+    if (request.method === 'GET' && !isEmty(data)) {
+        request.url += buildData(data);
+    }
+    if (request.method === 'POST') {
+        if (typeof (request.headers['content-type']) === 'undefined') {
+            request.headers['content-type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+        }
+        request.body = buildData(data);
+    }
+    fetch(new Request(request.url, request)).then(function (response) {
+        try {
+            response.json().then(function (resData) {
+                callback(resData);
+            }, errorCallback);
+        } catch (e) {
+            errorCallback(e);
+        }
+    }, errorCallback);
+}
+
+module.exports = ajax;
